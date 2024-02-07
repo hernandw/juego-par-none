22e32fa87f0deabba2b554992bcf3adf187f3afa
Williams Hernández <hernandw@gmail.com>
2024-02-07 17:12:32 -0300

    :bux: cambio del ternario por condicional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,38 @@
 //crear un programa que solicite al usuario un numero de dedos y opción
 
-let dedos = +prompt('indique un numero de dedos (entre 1 y 10)')
-let opcion = prompt('indique la opción (par o none)')
+let dedos = +prompt("indique un numero de dedos (entre 1 y 10)");
+let opcion = prompt("indique la opción (par o none)");
 
-if ((dedos >= 1 && dedos <= 10) && (opcion == 'par' || opcion == 'none')) {
-    
+if (dedos >= 1 && dedos <= 10 && (opcion == "par" || opcion == "none")) {
+  //generamos un numero aleatorio entre 1 y 10 para el jugador 2
 
-    //generamos un numero aleatorio entre 1 y 10 para el jugador 2
+  let dedos2 = Math.floor(Math.random() * 10) + 1;
 
-    let dedos2 = Math.floor(Math.random() * 10) + 1;
-    
-    console.log('la opcion de la pc es: ', dedos2)
+  
 
+  //calculamos la suma de los dedos
+  let suma = dedos + dedos2;
+  //comprobar si es par o impar
 
-    //calculamos la suma de los dedos
-    let suma = dedos + dedos2
-    //comprobar si es par o impar
+  /* let resultado = suma % 2 == 0 ? "par" : "none"; */
 
-    let resultado = suma % 2 == 0
-        ? 'par'
-        : 'none'
+  let resultado = ''
+  if (suma % 2 == 0) {
+    resultado = 'par'
+  } else {
+    resultado = 'none'
+  }
 
-    //mostramos el resultado
-    alert(`La suma de los dedos es: ${suma} y es ${resultado}`)
+  //mostramos el resultado
+  alert(`La suma de los dedos es: ${suma} y es ${resultado}`);
 
-    //compramos el resultado con la opcion del jugador 1
+  //compramos el resultado con la opcion del jugador 1
 
-    if (resultado == opcion) {
-        alert('Tu has ganado')
-    } else {
-        alert('ha ganado la computadora')
-    }
-
-
-
-
-
-
+  if (resultado == opcion) {
+    alert("Tu has ganado");
+  } else {
+    alert("ha ganado la computadora");
+  }
 } else {
-    alert('Ingrese datos validos')
+  alert("Ingrese datos validos");
 }
